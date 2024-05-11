322f677a859918e72bb3f2af30accb64f3569a6b
Jongok Yoon <yoonjongok1209@gmail.com>
2024-05-11 19:31:29 +0200

    rename confirm password

--- src/components/shared/BenchlinkRouter/index.tsx
+++ src/components/shared/BenchlinkRouter/index.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ScrollToTop from "../ScrollToTop";
 import DeleteAccount from "@/pages/DeleteAccount";
+import ConfirmPassword from "@/pages/DeleteAccount/ConfirmPassword";
 
 const BenchlinkRouter = () => {
 	return (
 		<BrowserRouter>
 			<ScrollToTop />
 			<Routes>
-				<Route path="/delete-account" element={<DeleteAccount />} />
+				<Route path="/delete-account">
+					<Route element={<DeleteAccount />} index />
+					<Route path="confirm-password" element={<ConfirmPassword />} />
+				</Route>
 
 				<Route path="/" element={<Navigate to="/delete-account" replace />} />
 				<Route path="*" element={<Navigate to="/delete-account" replace />} />
