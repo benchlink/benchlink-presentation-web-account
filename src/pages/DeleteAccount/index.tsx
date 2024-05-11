edddaecdd013d059e5dcc2623cb1c2261f1759e1
Jongok Yoon <yoonjongok1209@gmail.com>
2024-05-11 19:31:43 +0200

    Finish UI of delete account flow

--- src/pages/DeleteAccount/index.tsx
+++ src/pages/DeleteAccount/index.tsx
@@ -1,25 +1,26 @@
-import { ApiStatus } from "@/api/types/@shared";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 import LogoIcon from "@/assets/icons/benchlink-logo.svg?react";
 import FormItem from "@/components/shared/FormItem";
 import Button from "@/components/shared/atoms/Button";
-import Typography from "@/components/shared/atoms/Typography";
 import Input from "@/components/shared/atoms/Input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { z } from "zod";
+import Typography from "@/components/shared/atoms/Typography";
+import { ApiStatus } from "@/api/types/@shared";
 
-const passwordFormSchema = z.object({
-	password: z
+const emailFormSchema = z.object({
+	email: z
 		.string()
-		.min(8, { message: "Password must be at least 8 characters" }),
+		.min(1, { message: "Please enter the email" })
+		.email({ message: "Invalid email format" }),
 });
 
-export type PasswordFormValues = z.infer<typeof passwordFormSchema>;
+export type EmailFormValues = z.infer<typeof emailFormSchema>;
 
 const DeleteAccount = () => {
 	const [apiStatus, setApiStatus] = useState<ApiStatus>("idle");
-	const [isDeleted, setIsDeleted] = useState(true);
+	const [requestDelete, setRequestDelete] = useState(false);
 
 	// const mockPasswordUpdateRequestStatus = async (uuid: string) => {
 	// 	try {
@@ -60,22 +61,22 @@ const DeleteAccount = () => {
 		register,
 		formState: { errors, isValid },
 		setError,
-	} = useForm<PasswordFormValues>({
+	} = useForm<EmailFormValues>({
 		mode: "onSubmit",
-		resolver: zodResolver(passwordFormSchema),
+		resolver: zodResolver(emailFormSchema),
 		defaultValues: {
-			password: "",
+			email: "",
 		},
 	});
 
 	const btnDisabled = Object.keys(errors).length > 0 || !isValid;
 
-	const handleFormSubmit: SubmitHandler<PasswordFormValues> = async ({
-		password,
+	const handleFormSubmit: SubmitHandler<EmailFormValues> = async ({
+		email,
 	}) => {
 		try {
 			setApiStatus("loading");
-			console.log({ password });
+			console.log({ email });
 			// const result = await NetworkService.getHostByInviteCode({ inviteCode });
 
 			// if (!result.host) {
@@ -92,46 +93,57 @@ const DeleteAccount = () => {
 
 			// setApiStatus("succeeded");
 			// setHostInfoDrawerOpen(true);
-			setIsDeleted(true);
+			setRequestDelete(true);
 		} catch (error) {
 			setApiStatus("failed");
-			setError("password", { message: "Invalid password" });
+			setError("email", { message: "This account does not exist" });
 		}
 	};
 
 	return (
-		<div style={{ height: "100dvh" }}>
+		<div className="overflow-hidden" style={{ height: "100dvh" }}>
 			<header className="bg-[#000000]">
 				<div className="h-[58px] max-w-[694px] px-4  flex items-center justify-between mx-auto">
 					<LogoIcon width={123} />
 				</div>
 			</header>
 			<div
-				className="flexCol items-center  px-5 mt-4 gap-6"
+				className="flexCol items-center  px-5 mt-5 gap-6"
 				style={{ height: `calc(100dvh - 58px)` }}
 			>
-				{isDeleted ? (
-					<Typography type="h2">Your Benchlink account is deleted</Typography>
+				{requestDelete ? (
+					<div className="flexCol gap-5">
+						<Typography type="h2">We sent you an email</Typography>
+						<Typography type="h4" className="font-normal">
+							Please find the email in your inbox and finalize the deletion
+							process.
+						</Typography>
+					</div>
 				) : (
 					<>
-						<Typography type="h2">
-							Please enter your account password for xxx@gmail.com
-						</Typography>
+						<div className="flexCol gap-2 ">
+							<Typography type="h2">Delete my account</Typography>
+							<Typography type="caption2" className="text-gray-subText">
+								Once you delete your Benchlink account, all your existing
+								account information including your profile, network, and rewards
+								will be gone and cannot be redeemed.
+							</Typography>
+						</div>
 						<form
 							onSubmit={handleSubmit(handleFormSubmit)}
 							className="w-full space-y-5"
 						>
 							<FormItem
-								label="Invitation code"
-								error={errors.password?.message}
+								label="Email (Benchlink account)"
+								error={errors.email?.message}
 								required
 								initialFocus
 							>
-								<Input {...register("password")} />
+								<Input {...register("email")} />
 							</FormItem>
 							<div className="w-full bg-white outline-none text-center">
 								<Button
-									value="Delete Account"
+									value="Request deletion"
 									disabled={btnDisabled}
 									loading={apiStatus === "loading"}
 								/>
