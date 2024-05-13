b902ad1e657f1b7d2316a2466501dc9e8d9ca9db
Victor Joo <victor@frontli.ne.kr>
2024-05-14 07:24:55 +0900

    FIX window.parent -> window

--- src/components/shared/BenchlinkRouter/index.tsx
+++ src/components/shared/BenchlinkRouter/index.tsx
@@ -3,16 +3,16 @@ import DeleteAccount from "@/pages/DeleteAccount";
 
 const BenchlinkRouter = () => {
 	const urlParams = new URLSearchParams(window.location.search);
-	const uuid = urlParams.get('uuid') ?? '';
+    const uuid = urlParams.get('uuid') ?? '';
 
-	return (
-		<div
-			className="w-full h-full flexCol bg-white"
-			style={{height: "100dvh"}}
-		>
-			{uuid? <ConfirmPassword uuid={uuid}/>:<DeleteAccount/>}
-		</div>
-	);
+    return (
+        <div
+            className="w-full h-full flexCol bg-white"
+            style={{height: "100dvh"}}
+        >
+            {uuid ? <ConfirmPassword uuid={uuid}/> : <DeleteAccount/>}
+        </div>
+    );
 };
 
 export default BenchlinkRouter;
